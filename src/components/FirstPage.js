7162347ef5caed89b8fae5e0f934e68f55ac6a8b
Sara Fragnito <sara.fragnito@alten.it>
2022-07-04 11:30:46 +0200

    items inseriti in un file a parte

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,24 +1,10 @@
 import React, {useState} from "react"
 import Card from "./card";
 import ConfirmButton from "./confirmButton"
-//import { ITEMS } from "./data/items"
-
-import california from '../images/california.png';
-import dragon from '../images/dragon.png';
-import dynamite from '../images/dynamite.png';
-import philadelphia from '../images/philadelphia.png';
-import rainbow from '../images/rainbow.png';
-import shrimp from '../images/shrimp.png';
+import { ITEMS } from "../data/items"
 
 function FirstPage(){
-  const [cards, setCards] = useState([
-    { id: 0, nome: "California", porzione: "6 pz.", prezzo: 1.99, immagine: california, quantita: 0 },
-    { id: 1, nome: "Dragon", porzione: "4 pz.", prezzo: 2.99, immagine: dragon, quantita: 0 },
-    { id: 2, nome: "Dynamite", porzione: "8 pz.", prezzo: 3.99, immagine: dynamite, quantita: 0 },
-    { id: 3, nome: "Philadelphia", porzione: "6 pz.", prezzo: 4.99, immagine: philadelphia, quantita: 0 },
-    { id: 4, nome: "Rainbow", porzione: "8 pz.", prezzo: 5.99, immagine: rainbow, quantita: 0 },
-    { id: 5, nome: "Shrimp", porzione: "6 pz.", prezzo: 6.99, immagine: shrimp, quantita: 0 },
-  ]);
+  const [cards, setCards] = useState(ITEMS);
 
   const IncrementaQuantita = card => {
     const newCard = [...cards]
--- src/data/items.js
+++ src/data/items.js
@@ -1,8 +1,50 @@
-export const items = [
-  { id: 0, nome: "California", porzione: "6 pz.", prezzo: 1.99, immagine: california, quantita: 0 },
-  { id: 1, nome: "Dragon", porzione: "4 pz.", prezzo: 2.99, immagine: dragon, quantita: 0 },
-  { id: 2, nome: "Dynamite", porzione: "8 pz.", prezzo: 3.99, immagine: dynamite, quantita: 0 },
-  { id: 3, nome: "Philadelphia", porzione: "6 pz.", prezzo: 4.99, immagine: philadelphia, quantita: 0 },
-  { id: 4, nome: "Rainbow", porzione: "8 pz.", prezzo: 5.99, immagine: rainbow, quantita: 0 },
-  { id: 5, nome: "Shrimp", porzione: "6 pz.", prezzo: 6.99, immagine: shrimp, quantita: 0 },
+export const ITEMS = [
+  { 
+    id: 0, 
+    nome: "California", 
+    porzione: "6 pz.", 
+    prezzo: 1.99, 
+    immagine: '../images/california.png', 
+    quantita: 0 
+  },
+  { 
+    id: 1, 
+    nome: "Dragon", 
+    porzione: "4 pz.", 
+    prezzo: 2.99, 
+    immagine: '../images/dragon.png', 
+    quantita: 0 
+  },
+  { 
+    id: 2, 
+    nome: "Dynamite", 
+    porzione: "8 pz.", 
+    prezzo: 3.99, 
+    immagine: '../images/dynamite.png', 
+    quantita: 0 
+  },
+  { 
+    id: 3, 
+    nome: "Philadelphia", 
+    porzione: "6 pz.", 
+    prezzo: 4.99, 
+    immagine: '../images/philadelphia.png', 
+    quantita: 0 
+  },
+  { 
+    id: 4, 
+    nome: "Rainbow", 
+    porzione: "8 pz.", 
+    prezzo: 5.99, 
+    immagine: '../images/rainbow.png', 
+    quantita: 0 
+  },
+  { 
+    id: 5, 
+    nome: "Shrimp", 
+    porzione: "6 pz.", 
+    prezzo: 6.99, 
+    immagine: '../images/shrimp.png', 
+    quantita: 0 
+  },
 ]
