4abe9fcceb60536fff8b2bfefab1e8b21d0cff59
Sara Fragnito <sara.fragnito@alten.it>
2022-07-06 09:04:32 +0200

    sistemato footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="py-3 my-6 footer footer-dark bg-dark fixed-bottom" >
+        <footer className="py-2 footer footer-dark bg-dark" >
             <p className="text-center text-muted">&copy; Shimazu 2021 Company, Inc</p>
         </footer>
     )
